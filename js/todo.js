c238319045338d5886fffd2ac678ce36ae4914da
hitit1000 <hitit1000@naver.com>
2022-03-14 17:42:01 +0900

    update write todo

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,29 @@ const toDoForm = document.querySelector("#todo_form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo_list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+const saveToDos = () => {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+    console.log("hi")
+}
+
+const deleteToDo = (event) => {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 const paintToDo = (value) => {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = value;
-    console.log(li);
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -16,6 +33,15 @@ const handleToDoSubmit = (event) => {
     const newTodo = toDoInput.value;
     toDoInput.value = "";
     paintToDo(newTodo);
+    toDos.push(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos)
+if(savedToDos){
+    const parsedToDos = JSON.parse(savedToDos);
+    console.log(parsedToDos)
+}
